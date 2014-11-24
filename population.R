622731419f40766aee1ad50700a5004d4a1e95c0
BCE provisioner <bce@lists.berkeley.edu>
2014-11-23 22:06:33 -0800

    population init

---
+++ population.R
@@ -0,0 +1,15 @@
+##### Population OOP Code #####
+##### Description: #####
+### Here we construct the basic S4 Class: Population ###
+
+setClass("Population", 
+         representation(encoding = "character", popSize = "numeric", geneLength = "numeric", 
+                        genePool = "matrix", fit = "vector", generation = "numeric"), 
+         prototype(encoding = "binary", popSize = 0, geneLength = 0, genePool = matrix(nrow = 0, ncol = 0),
+                   fit = c(), generation = 0)
+)
+
+
+
+
+
